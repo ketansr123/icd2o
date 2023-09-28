f6b363c5332aa19bac4b49e9bfad398c3782b003
Ketan Rawat <krawat@bayviewglen.ca>
2023-09-28 13:54:51 -0400

    moved folders

--- Unit One _ Foundation of Python Programming/day 5 homework/hw1.py
+++ Unit One _ Foundation of Python Programming/day 5 homework/hw1.py
@@ -5,4 +5,4 @@ dessert= float(input("How much does the dessert cost"))
 tip= int(input("What percentage do you tip? (Dont type the %)"))
 cost=drink+appetizer+entree+dessert
 total=cost+tip*0.01
-print(f"The total cost of your meal was {total}$")
+print(f"The total cost of your delicious good meal was {total}$")
