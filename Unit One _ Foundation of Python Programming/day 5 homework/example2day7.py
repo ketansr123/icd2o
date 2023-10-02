582db1ed75111e895b976064f16b6f3339b959d7
Ketan Rawat <krawat@bayviewglen.ca>
2023-10-02 14:56:12 -0400

    learning how distance different things

---
+++ Unit One _ Foundation of Python Programming/day 5 homework/example2day7.py
@@ -0,0 +1,13 @@
+print("12345678901234567890")
+
+name = "steve"
+print(f"{name}")
+print(f"{name:>20}")
+print(f"{name:>10}")
+print(f"{name:<10}Dev")
+print(f"{name:^20}")
+
+
+
+x = 59.1543155134
+print(f"{x:.1}")
---
+++ Unit One _ Foundation of Python Programming/day 5 homework/example3.py
@@ -0,0 +1,9 @@
+average = 67.445453
+
+print(f"{average}")
+print(f"{average:.2f}")
+print(f"{average:>20}")
+print(f"{average:>20.2f}")
+print(f"{average:<20.2f}dev")
+
+print(f"{average:>10.2f}")
