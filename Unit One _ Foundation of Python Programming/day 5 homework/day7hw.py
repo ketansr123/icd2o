d0f6b0fed08f5856c6eec460806f982dc6f2e6a9
Ketan Rawat <krawat@bayviewglen.ca>
2023-10-04 13:27:54 -0400

    practicing f strngs.

---
+++ Unit One _ Foundation of Python Programming/day 5 homework/day7hw.py
@@ -0,0 +1,19 @@
+#question 1
+import math
+x = math.pi
+print(f"{x:.3f}")
+#question 2
+y = 29.99
+print(f"${y:.2f}")
+#percentage and decimal places 1
+xx=0.075
+print(f"{xx:.2%}")
+#question 2
+xy=0.25
+print(f"{xy:.1%}")
+#width question 1 
+city="new york"
+print(f"{city:<15}blahblahblah")
+#width question 2 
+temperature=72.5
+print(f"{temperature:^10}")
